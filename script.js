34977ce06452440e0b0090853ac57aa4943dc501
kanitkm <kanit@kmschool.ac.th>
2026-08-17 11:06:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 document.addEventListener('DOMContentLoaded',function () {
-  document.getElementById('year').innerHTML = new Date().getFullYear();
   console.log('Portfolio ของ Kanit พร้อมแล้ว!');
 });
